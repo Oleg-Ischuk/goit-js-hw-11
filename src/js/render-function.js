7a5198ce52fe902853f7a172fc054ef757a880cd
Oleg_Ischuk <ipz231_ios@student.ztu.edu.ua>
2025-01-16 18:31:14 +0200

    changes in style

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -12,27 +12,29 @@ export function handleSuccess(data) {
       }) => {
         return `<li class="gallery-item">
           <article class="card">
-      <a class="card-link" href="${largeImageURL}"><img class="card-image" src="${webformatURL}" alt="${tags}"/></a>        
-      <div class="card-container">
-            <div class="card-item">
-              <p class="card-title">Likes</p>
-              <p class="card-count">${likes}</p>
+            <a class="card-link" href="${largeImageURL}" target="_blank" rel="noopener noreferrer">
+              <img class="card-image" src="${webformatURL}" alt="${tags}" />
+            </a>
+            <div class="card-container">
+              <div class="card-item">
+                <p class="card-title"><i class="fas fa-heart"></i> Likes</p>
+                <p class="card-count">${likes}</p>
+              </div>
+              <div class="card-item">
+                <p class="card-title"><i class="fas fa-eye"></i> Views</p>
+                <p class="card-count">${views}</p>
+              </div>
+              <div class="card-item">
+                <p class="card-title"><i class="fas fa-comment"></i> Comments</p>
+                <p class="card-count">${comments}</p>
+              </div>
+              <div class="card-item">
+                <p class="card-title"><i class="fas fa-download"></i> Downloads</p>
+                <p class="card-count">${downloads}</p>
+              </div>
             </div>
-            <div class="card-item">
-              <p class="card-title">Views</p>
-              <p class="card-count">${views}</p>
-            </div>
-            <div class="card-item">
-              <p class="card-title">Comments</p>
-              <p class="card-count">${comments}</p>
-            </div>
-            <div class="card-item">
-              <p class="card-title">Downloads</p>
-              <p class="card-count">${downloads}</p>
-            </div>
-          </div>
           </article>
-            </li>`;
+        </li>`;
       }
     )
     .join('');
